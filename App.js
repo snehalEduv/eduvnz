5d0f82349da7aba2e013d242b42c3f05ddaa5410
Vinayak0227 <75468546+Vinayak0227@users.noreply.github.com>
2020-12-06 19:20:30 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,26 @@
+import Carosel from './Carosel/Carosel';
+import './App.css';
+import React, { Component } from 'react';
+class App extends Component{
+
+
+  render() {
+    return (
+    <div className = "App">
+    <Carosel/>
+    
+    
+    </div>
+    );
+
+
+      
+    
+
+
+
+
+  }
+}
+
+export default App;
